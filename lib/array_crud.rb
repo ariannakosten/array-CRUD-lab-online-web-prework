30f2d4c277516df0b4633d515a0f6d950b414210
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-15 07:35:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  months << "October"
+  months = ["June", "July", "August", "September"]
 end
 
 def add_element_to_start_of_array(array, element)
